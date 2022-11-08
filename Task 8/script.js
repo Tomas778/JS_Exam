8b0a2d0239baed37bb23ca8a176b930f1aa23e1d
Tomas Stirblys <eternadeliux25@gmail.com>
2022-11-08 18:58:02 +0200

    DevBranch update Task 8 - done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,17 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+//Constructor
+function Calculator (a, b) {
+    this.a = a;
+    this.b = b;
+    this.sum = a+b;
+    this.sub = a-b;
+    this.mul = a*b;
+    this.div = a/b;
+}
+
+const calc1 = new Calculator(10, 5);
+
+console.log(calc1);
